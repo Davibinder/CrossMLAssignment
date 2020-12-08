42044670ca1e90ffd72b0a6a86fee4fef8742a88
Davinder Singh <dsingh@chicmic.co.in>
2020-12-08 15:32:19 +0530

    unit test for home page
    unit test for home page

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from main_app.services.wordTagGenerator import genrateWordTagsFromText
 from main_app.services.pdfHandler import getPdfRawText
 from django.conf import settings
@@ -7,6 +7,9 @@ from django.conf import settings
 
 class GenralTest(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
     def test_testWordCloudService(self):
         base64Data = genrateWordTagsFromText('aa bb cc  aa aa aa aa aa')
         self.assertTrue(base64Data)
@@ -14,3 +17,8 @@ class GenralTest(TestCase):
     def test_extractrawTextFromPdf(self):
         rawText = getPdfRawText(settings.MEDIA_ROOT+'/example.pdf')
         self.assertTrue(rawText)
+
+    def test_homepage(self):
+        homepage = self.client.get('')
+        self.assertTrue(homepage.status_code, 200)
+        self.assertTemplateUsed(homepage, 'upload.html')
